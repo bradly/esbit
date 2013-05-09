62863e43a8bcf3be036aa78cbdc9859936c361d4
Bradly Feeley <bradly_feeley@intuit.com>
2013-05-09 15:13:23 -0700

    Adding more specs for Campfire#rooms.

--- spec/esbit/campfire_spec.rb
+++ spec/esbit/campfire_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe Esbit::Campfire do
-  describe "initialization" do
-    let(:campfire) { Esbit::Campfire.new('subdomain', 'token') }
+  let(:campfire) { Esbit::Campfire.new('subdomain', 'token') }
 
+  before(:each) do
+    RestClient.stub(:get).and_return(MOCK_ROOMS_API_RESPONSE)
+  end
+
+  describe "initialization" do
     it "should accept a subdomain and token" do
       campfire.subdomain.should eql 'subdomain'
       campfire.token.should eql 'token'
@@ -11,6 +15,27 @@ describe Esbit::Campfire do
 
     it "should setup a connection" do
       campfire.connection.should be_a Esbit::Connection
+    end
+  end
+
+  describe "rooms" do
+    it "should return an array of rooms" do
+      campfire.rooms.all? { |room| room.is_a? Esbit::Room }.should be_true
+    end
+
+    it "should only make one request" do
+      RestClient.should_receive(:get).once
+      3.times { campfire.rooms }
+    end
+  end
+
+  describe "finding rooms" do
+    it "should find a room by id" do
+      campfire.find_room_by_id(1).name.should eql "Chat Room 1"
+    end
+
+    it "should find a room by name" do
+      campfire.find_room_by_name("Chat Room 2").id.should eql 2
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,5 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.order = 'random'
 end
+
+MOCK_ROOMS_API_RESPONSE = "{\"rooms\":[{\"updated_at\":\"2012/10/09 23:57:53 +0000\",\"topic\":\"Chat Room 1\",\"name\":\"Chat Room 1\",\"membership_limit\":25,\"created_at\":\"2012/01/26 06:25:58 +0000\",\"locked\":false,\"id\":1},{\"updated_at\":\"2012/06/13 13:25:52 +0000\",\"topic\":\"Chat Room 2\",\"name\":\"Chat Room 2\",\"membership_limit\":25,\"created_at\":\"2012/01/26 06:40:14 +0000\",\"locked\":false,\"id\":2}]}"
